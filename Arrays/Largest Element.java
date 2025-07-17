64b9ecfda0c7d1ac4f67a55a67a686e0e2bd61f8
Riya Kushwah <120267693+Riyaaaaaaaaaaaaa@users.noreply.github.com>
2025-07-17 18:03:41 +0530

    Create Largest Element.java

---
+++ Arrays/Largest Element.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int largestElement(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for(int i = 0 ; i < nums.length ; i++) {
+            if(max < nums[i]) {
+                max = nums[i];
+            }
+        }
+        return max;
+    }
+}
