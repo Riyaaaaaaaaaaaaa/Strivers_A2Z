aed3c9aca9137ba8a64b98847b744921dc8aa888
Riya Kushwah <120267693+Riyaaaaaaaaaaaaa@users.noreply.github.com>
2025-07-20 20:18:44 +0530

    Update Two Sum.java

--- Arrays/Two Sum.java
+++ Arrays/Two Sum.java
@@ -1,1 +1,18 @@
-
+//to check the sum is in the array or not(for sorted arrays)
+class Solution {
+    public boolean twoSum(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right) {
+            int sum = nums[left] + nums[right];
+            if(sum == target) {
+                return true;
+            } else if(sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return false;
+    }
+}
