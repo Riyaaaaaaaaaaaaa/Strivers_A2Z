57524b1da550507979e011a36962b4b9722aeffa
Riya Kushwah <120267693+Riyaaaaaaaaaaaaa@users.noreply.github.com>
2025-07-25 18:23:08 +0530

    Create Reverse String.java

---
+++ String/Reverse String.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] str) {
+        int left = 0;
+        int right = str.length - 1;
+
+        while(left < right) {
+            char temp = str[left];
+            str[left++] = str[right];
+            str[right--] = temp;
+        }
+        for(char string: str) {
+            System.out.print(string);
+        }
+    }
+}
