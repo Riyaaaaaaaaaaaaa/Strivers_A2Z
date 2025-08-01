b1db602fee0536a8e5c04d5e448f181917b7795b
Riya Kushwah <120267693+Riyaaaaaaaaaaaaa@users.noreply.github.com>
2025-08-01 14:09:52 +0530

    Create Delete head of DLL.java

---
+++ Linked List/Delete head of DLL.java
@@ -0,0 +1,8 @@
+class Solution {
+    public ListNode deleteHead(ListNode head) {
+        if(head == null || head.next == null) return null;
+        head = head.next;
+        head.prev = null;
+        return head;
+    }
+}
