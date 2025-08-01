00452fecb4714d7ce0e19c9745b76f570883be61
Riya Kushwah <120267693+Riyaaaaaaaaaaaaa@users.noreply.github.com>
2025-08-01 14:16:20 +0530

    Create Reverse a LL.java

---
+++ Linked List/Reverse a LL.java
@@ -0,0 +1,14 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+
+        while(curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+}
