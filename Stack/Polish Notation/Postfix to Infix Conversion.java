02b66d7127b6b24d1712e2f66f73912f4057d4c1
Riya Kushwah <120267693+Riyaaaaaaaaaaaaa@users.noreply.github.com>
2025-08-02 22:24:33 +0530

    Create Postfix to Infix Conversion.java

---
+++ Stack/Polish Notation/Postfix to Infix Conversion.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String postfixToInfix(String s) {
+        // Your code goes here
+        Stack<String> stack = new Stack<>();
+
+        for(int i = 0 ; i < s.length() ; i++) {
+            char ch = s.charAt(i);
+            if(Character.isLetterOrDigit(ch)) {
+                stack.push(Character.toString(ch));
+            } else {
+                String op1 = stack.pop();
+                String op2 = stack.pop();
+                String temp = "(" + op2 + ch + op1 +")";
+                stack.push(temp);
+            }
+        }
+        return stack.peek();
+    }
+}
